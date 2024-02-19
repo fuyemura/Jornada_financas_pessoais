314b464e7871d64a1667c85130d90fba54cda6a4
fuyemura <fuyemura@gmail.com>
2024-02-19 12:25:10 -0300

    Versão do etl para pyspark

--- etl/Extracao_cotahist_pyspark.py
+++ etl/Extracao_cotahist_pyspark.py
@@ -1,3 +1,4 @@
+
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import substring
 from datetime import date
@@ -11,14 +12,14 @@ spark = SparkSession.builder \
 
 # Define as posições iniciais e finais de cada campo
 positions = {"tipo_registro": (1, 2),
-               "data_pregao": (2, 10),
-               "codigo_bdi": (10, 12),
-               "codigo_negociacao": (12, 18),
+               "data_pregao": (3, 10),
+               "codigo_bdi": (11, 12),
+               "codigo_negociacao": (13, 24),
                "tipo_mercado": (25, 27),
-               "nome_resumido_empresa": (27, 37),
-               "especificacao_papel": (38, 49),
-               "prazo_dias_mercado": (50, 51),
-               "moeda_referencia": (52, 56),
+               "nome_resumido_empresa": (28, 39),
+               "especificacao_papel": (40, 49),
+               "prazo_dias_mercado": (50, 52),
+               "moeda_referencia": (53, 56),
                "preco_abertura_papel": (57, 69),
                "preco_maximo_papel": (70, 82),
                "preco_minimo_papel": (83, 95),
@@ -30,12 +31,12 @@ positions = {"tipo_registro": (1, 2),
                "quantidade_total_titulos": (153, 170),
                "volume_total_titulos": (171, 188),
                "preco_exercicio_opcoes": (189, 201),
-               "indicador_correcao_precos": (200, 202),
+               "indicador_correcao_precos": (202, 202),
                "data_vencimento_opcoes": (203, 210),
                "fator_cotacao_papel": (211, 217),
-               "preco_exercicio_pontos": (218, 229),
-               "codigo_papel_sistema": (230, 240),
-               "numero_distribuicao_papel": (242, 245)
+               "preco_exercicio_pontos": (218, 230),
+               "codigo_papel_sistema": (231, 242),
+               "numero_distribuicao_papel": (243, 245)
 			   }
 
 # Caminho do arquivo de entrada
@@ -48,21 +49,23 @@ df = spark.read.text(file_path)
 for field, (start, end) in positions.items():
     df = df.withColumn(field, substring("value", start, end - start + 1))
 
+# Filtrar os registros com tipo do registro igual a 1
+df = df.filter(df["tipo_registro"] == "01")    
+
 # Remove a coluna "value" original
 df = df.drop("value")
 
-
 # Configurações para conexão com o banco de dados MySQL
 jdbc_url = "jdbc:mysql://localhost:3306/bronze"
-mode = "overwrite"  # Sobrescreve a tabela se ela já existir
-properties = {
+jdbc_mode = "overwrite"  # Sobrescreve a tabela se ela já existir
+jdbc_properties = {
     "user": "financasp",
     "password": "Financasp#321",
     "driver": "com.mysql.cj.jdbc.Driver"
 }
 
 # Escreve o DataFrame no banco de dados MySQL
-df.write.jdbc(url=jdbc_url, table="cotahist", mode=mode, properties=properties)
+df.write.jdbc(url=jdbc_url, table="cotahist", mode=jdbc_mode, properties=jdbc_properties)
 
 # Mostra o DataFrame resultante
 #df.show()
