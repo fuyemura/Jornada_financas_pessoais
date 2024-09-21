ba848446357f11246b19ed337f8bc867cda6d009
fuyemura <fuyemura@gmail.com>
2024-09-21 20:55:40 -0300

    Ajustes na estrutura do projeto

--- pipeline/carga_silver_cotacoes_historicas.py
+++ pipeline/carga_silver_cotacoes_historicas.py
@@ -1,12 +1,19 @@
 import pandas as pd
 from conexao_banco_de_dados import conexao_mysql
-
-# Consulta SQL:
-consulta = pd.read_sql_table('cotahist', con=conexao_mysql('bronze'))
+# cotahist SQL:
+cotahist = pd.read_sql_query('SELECT*FROM bronze.cotahist', con=conexao_mysql('bronze'))
+cotahist['data_pregao'] = pd.to_datetime(cotahist['data_pregao'], format='%Y%m%d')
+cotahist['preco_abertura_papel'] = cotahist['preco_abertura_papel']/100
+cotahist['preco_maximo_papel'] = cotahist['preco_maximo_papel']/100
+cotahist['preco_minimo_papel'] = cotahist['preco_minimo_papel']/100
+cotahist['preco_medio_papel'] = cotahist['preco_medio_papel']/100
+cotahist['preco_ultimo_negocio'] = cotahist['preco_ultimo_negocio']/100
+cotahist['preco_melhor_oferta_compra'] = cotahist['preco_melhor_oferta_compra']/100
+cotahist['preco_melhor_oferta_venda'] = cotahist['preco_melhor_oferta_venda']/100
 
 # Conexão com o banco de dados na camada "silver":
 engine = conexao_mysql('silver')
-data_frame = consulta.to_sql(name='cotahist', con=engine, if_exists='replace', index=False)
+data_frame = cotahist.to_sql(name='cotacoes_historicas', con=engine, if_exists='replace', index=False)
 if (data_frame):
     print('\033[1;32mDados gravados com sucesso!\033[m')
 else:
