1cdf59caf7f21e440b6ff6d04a2cc6fe3e8f17f8
fuyemura <fuyemura@gmail.com>
2024-10-05 22:01:31 -0300

    ajustes no pipeline de carga

--- pipeline/carga_silver_cotacoes_historicas_pyspark.py
+++ pipeline/carga_silver_cotacoes_historicas_pyspark.py
@@ -1,10 +1,13 @@
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import to_date, col
+from pyspark.sql.types import DecimalType
 from conexao_bancodedados import *
 
 # Criação da SparkSession
 spark = SparkSession.builder \
     .appName("Leitura e Transformação MySQL") \
+    .config("spark.executor.memory", "4g") \
+    .config("spark.driver.memory", "2g") \
     .getOrCreate()
 
 # Configurações para conexão com o banco de dados MySQL
@@ -24,7 +27,8 @@ df_transformado = df.withColumn("data_pregao", to_date(col("data_pregao"), "yyyy
                     .withColumn("preco_melhor_oferta_compra", col("preco_melhor_oferta_compra").cast("float") / 100) \
                     .withColumn("preco_melhor_oferta_venda", col("preco_melhor_oferta_venda").cast("float") / 100) \
                     .withColumn("volume_total_titulos", col("volume_total_titulos").cast("float") / 100) \
-                    .withColumn("preco_exercicio_opcoes", col("preco_exercicio_opcoes").cast("float") / 100)
+                    .withColumn("preco_exercicio_opcoes", col("preco_exercicio_opcoes").cast("float") / 100) \
+                    .withColumn("preco_exercicio_pontos", col("preco_exercicio_pontos").cast(DecimalType(7, 6)) / 1000000)
 
 # Truncar os dados da tabela antes de carregar
 truncate_table('silver','cotacoes_historicas')
