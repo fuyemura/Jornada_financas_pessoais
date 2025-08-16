4ea0457ceae26540200c3a381ca65775cca83f06
Flávio Uyemura <fuyemura@hotmail.com>
2025-08-16 19:04:46 -0300

    alteração dos pipelines para pyspark

--- pipeline/conexao_bancodedados.py
+++ pipeline/conexao_bancodedados.py
@@ -1,23 +1,30 @@
-from sqlalchemy import create_engine, text
+import traceback
 
-def conexao_mysql(schema):
-    """Está função é usada para conectar o Pycharm ao banco de dados mysql, que está hospedado no servidor local:
-    :param = conector
-    :param = root
-    :param = password
-    :param = servidor
-    :param = database"""
-    return create_engine(f'mysql+mysqlconnector://financasp:Financasp#321@localhost/{schema}')
-
-def connection(database='bronze'):
+def connection_mysql(database='bronze'):
+    """
+    Função usada para obter a url de conexão do jdbc para o MySQL.
+    """
     jdbc_url = f"jdbc:mysql://localhost:3306/{database}"
     return jdbc_url
 
-def properties(user="financasp", password="Financasp#321", driver="com.mysql.cj.jdbc.Driver"):
+def properties_mysql(user="financasp", password="Financasp#321", driver="com.mysql.cj.jdbc.Driver"):
+    """
+    Função usada para obter usuário, senha e driver do jdbc para o MySQL.
+    """
     jdbc_properties = {"user":user,"password":password,"driver":driver}
     return jdbc_properties
 
-def truncate_table(database='bronze', tablename='cotahist'):
-    engine = conexao_mysql(database)
-    with engine.connect() as conexao:
-        conexao.execute(text(f"TRUNCATE TABLE {database}.{tablename}"))
+def write_pyspark_mysql(df, table_name, database='bronze', mode='append'):
+    """
+    Grava um DataFrame PySpark no MySQL.
+    """
+    jdbc_url = connection_mysql(database)
+    properties = properties_mysql()
+
+    try:
+        df.write.jdbc(url=jdbc_url, table=table_name, mode=mode, properties=properties)
+        print(f"[SUCESSO] DataFrame gravado na tabela '{table_name}' (mode={mode})!")
+
+    except Exception as e:
+        print(f"[ERRO ao gravar PySpark] {e}")
+        traceback.print_exc()
